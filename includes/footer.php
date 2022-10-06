58aa916d44fc6e30723e1f4d805840dd112205b6
favourloverboy <favourehio2019@gmail.com>
2022-10-06 10:33:32 +0100

    this is the MVC setup

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+        
+        <!-- Custom JavaScript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+    include('config/dblink.php');
+    $collect = new DB();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <base href="http://localhost/PHP-Test/">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- title -->
+        <title>Sign In Page</title>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
+
+        <!-- Custom styles for this template-->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div class="login">
+
+        </div>
+        
+        <!-- Custom JavaScript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
